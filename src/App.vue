cc55bcb4a9509941b87cac72fe6606ec8f80c845
Giric Goyal <giricgoyal@silicosense.com>
2022-01-28 11:45:38 +0530

    Remove unnecessary imports

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,12 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
 import Topbar from "./components/topbar/component.vue";
 import Page, { Sidebar, Content } from "./components/page/component.vue";
 import GetFreeInsurance from "./pages/get-free-insurance/component.vue";
 
 @Options({
   components: {
-    HelloWorld,
     Topbar,
     Page,
     Sidebar,
